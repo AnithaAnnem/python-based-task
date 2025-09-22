0aeeb7c113f620b36ba3d4d21678794894677c13
AnithaAnnem <anitha.annem.snaatak@mygurukulam.co>
2025-09-22 22:38:45 +0530

    Update core.py

--- samplemod/sample/core.py
+++ samplemod/sample/core.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
+from flask import Flask
 from . import helpers
 
+app = Flask(__name__)
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
-
-
-def hmm():
-    """Contemplation..."""
+@app.route('/')
+def home():
     if helpers.get_answer():
-        print(get_hmm())
+        return 'hmmm...'
+    else:
+        return 'No answer!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
